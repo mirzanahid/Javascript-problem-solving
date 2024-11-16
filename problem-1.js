1f1fb39dc9f7f7b9301ab102c41f95578b095cb2
mirzanahid <mirzasaikat3@gmail.com>
2024-11-16 11:29:39 +0600

    porject initiate

---
+++ problem-1.js
@@ -0,0 +1,11 @@
+// give an array of integers, replace all the occurrences of elementToReplace with substraction element.
+
+//  For inputArray = [1,2,1], elementToReplce = 1 and substractionElm = 3
+//  the output should be arrayReplace(inputArray, elementToReplace, substractionElem) = [3,2,3]
+
+function arrayReplace(inputArray, elementToReplace, sbustractionElem) {
+
+    const getDa = inputArray.find(data => data )
+
+}
+
