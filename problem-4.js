971cc139a10a03afe4a8b0a6f38b825dd275a7db
mirzanahid <mirzasaikat3@gmail.com>
2024-11-17 11:52:57 +0600

    problem 4 and 5 solution added

---
+++ problem-4.js
@@ -0,0 +1,22 @@
+// Give a sequence of integer to an array, determine whether it is possible to obtain a strictly increasing sequence by removing not more than one element from the array.
+
+//example
+//# For sequence = [ 1, 3, 2, 1], the output should be almostincreasingSequence(sequence) = false;
+
+//* There is no one element in this array that can be removed in order to get a strictly increasing sequence.
+
+//## For sequence = [ 1, 2, 3], the output should be almostincreasingSequence(sequence) = true;
+
+// * You can remove 3 from the array to get a strictly increasing sequence [1, 2]. Alternately, you can remove 2 to get the strictly increasing sequence [1, 3]
+
+function almostincreasingSequence(sequence) {
+  for (let i = 1; i < sequence.length; i++) {
+    if (sequence[i] < sequence[i-1] ) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const sequence = [1,2,3,5,6,9]
+console.log(almostincreasingSequence(sequence))
---
+++ problem-5.js
@@ -0,0 +1,19 @@
+//Sum all the prime numbers up to and including the provided number.
+
+//tipes: A prime number is defined as a number greater than one and having only two divisors, one and itself. For example, 2 is a prime number because it's only divisible by one and two.
+
+function primeTotal(num) {
+  let totalPrime = 0;
+  for (let i = 2; i < num; i++) {
+    for (let j = 2; j < num; j++) {
+      if (i === j) {
+        totalPrime += i;
+      }
+      if (i % j === 0) {
+        break;
+      }
+    }
+  }
+  return totalPrime;
+}
+console.log(primeTotal(10));
